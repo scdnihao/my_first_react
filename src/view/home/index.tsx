cd79c3b27f3f2ba2c9de5f1c2810e19bc14b75b4
scd <15209278757@163.com>
2022-03-08 21:11:39 +0800

    暴露webpack配置

--- src/view/home/index.tsx
+++ src/view/home/index.tsx
@@ -1,13 +1,30 @@
-import React,{useEffect} from "react";
+import React from "react";
 import './index.less'
 
-export default function Home(){
-    useEffect(()=>{
+// export default function Home(){
+//     useEffect(()=>{
 
-    },[])
-    return (
-        <>
-            <div id='home'> holle word<p className="one_p">123</p></div>
-        </>
-    )
+//     },[])
+//     return (
+//         <>
+//             <div id='home'> holle word<p className="one_p">123</p></div>
+//             <div>12344333333</div>
+//         </>
+//     )
+// }
+class Home extends React.Component  {
+
+    public change = ()=>{
+
+    }
+
+    render (){
+        return (
+            <>
+            <div>123321</div>
+            </>
+        )
+    }
 }
+
+export default Home
--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,54 @@
 const lessRegex = /\.less$/;
 const lessModuleRegex = /\.module\.less$/;
 const path = require('path');
+// const webpack=require('webpack')
 
+let htmlwebpackplugin = require('html-webpack-plugin');//引入html-webpack-plugin插件 
+
+let get_html = function(name,chunk){//封装
+    return {
+        template: './app/ejs/generate/'+ name + '.ejs',
+        filename:  name+ '.html',
+        chunks  : ['main',chunk||null],//这里引入公共文件main.js，另外一个文件按需引入，当然也可以把这个的值设为数组，传入function的第二个值用数组就行
+        chunksSortMode: 'manual',//将chunks按引入的顺序排序
+        inject  : true,//所有JavaScript资源插入到body元素的底部
+        hash    : true,
+		xhtml	: true
+    }
+};
 
 
 module.exports = {
         entry: './src/index.js',
         output:{
-            filename:'bundle.js',
-            path:path.resolve(__dirname,'dist')
+            path:path.join(__dirname,'./dist'),//指定打包好的文件，输出到哪个目录中去
+            filename:'bundle.js' //这是指定 输出的文件的名称
         },
-        module:{
-            rules:[
+        devServer: {
+            static: {
+              directory: path.join(__dirname, 'public'),
+            },
+            compress: true,
+            port: 9000,
+            inline:true
+          },
+        module: {
+            loaders: [
                 {
-                    test:/\.css$/,
-                    use:[
+                    test: /\.less$/,
+                    use: [
                         'style-loader',
-                        'css-loader'
+                        'css-loader',
+                        'less-loader' 
                     ]
-                },{
-                    test:/\.(png|svg|jpg|gif)$/,
-                    use:[
-                        'file-loader'
-                    ]
-                }
+                },
+                
             ]
-        }
-    }
+        },
+        plugins: [
+            //new一个模板出来测试一下
+   　　		new htmlwebpackplugin(get_html("home","main"))
+        ]
+   
+}
+
